f2ec33446915b946c536785831a37f96e759fc2b
DonShan <msdjshan47@gmail.com>
2022-12-19 17:33:29 +0530

    Initial Commit

---
+++ SwisftUI Networking MVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwisftUI Networking MVVM
+//
+//  Created by Madushan Senavirathna on 2022-12-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwisftUI Networking MVVM/SwisftUI_Networking_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwisftUI_Networking_MVVMApp.swift
+//  SwisftUI Networking MVVM
+//
+//  Created by Madushan Senavirathna on 2022-12-19.
+//
+
+import SwiftUI
+
+@main
+struct SwisftUI_Networking_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
